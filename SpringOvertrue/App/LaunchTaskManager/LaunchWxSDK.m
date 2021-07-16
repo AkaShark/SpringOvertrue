f9490620e4d1a3bba18d1731b5677a4302075bfb
Sharker <1548742234@qq.com>
2021-07-17 01:08:38 +0800

    接入微信登录SDK

--- SpringOvertrue/App/LaunchTaskManager/LaunchWxSDK.m
+++ SpringOvertrue/App/LaunchTaskManager/LaunchWxSDK.m
@@ -6,7 +6,30 @@
 //
 
 #import "LaunchWxSDK.h"
+#import "LaunchManager.h"
+#import <SOWXApiManager.h>
 
 @implementation LaunchWxSDK
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [[LaunchManager defaultManager] addTask:self];
+    }
+    return self;
+}
+
+- (NSString *)taskName {
+    return @"LaunchWxTask";
+}
+
+- (SOLaunchTaskPriority)taskProiority {
+    return SOLaunchTaskPriorityNormal;
+}
+
+- (void)run {
+    [[SOWXApiManager defaultManager] setupWXSDKWithApp:@"" universalLink:@""];
+    
+}
+
 
 @end
